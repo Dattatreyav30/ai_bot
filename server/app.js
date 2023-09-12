1153172687908b5dc9e108271d9a0c8d445c577a
dattatreyav30 <dattatreya361@gmail.com>
2023-09-13 00:41:02 +0530

    replacing with nosql databses

--- server/app.js
+++ server/app.js
@@ -20,7 +20,6 @@ app.get("/test", (req, res, next) => {
   res.status(200).json({ message: "succesfull" });
 });
 
-sequelize.sync();
 app.listen(5000, () => {
   console.log("port running on 5000");
 });
