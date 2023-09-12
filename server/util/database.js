960ccfddba245c56c94655d8aa23dec3c00519e2
dattatreyav30 <dattatreya361@gmail.com>
2023-09-12 21:17:54 +0530

    changes in databse thing

--- server/util/database.js
+++ server/util/database.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_ROOT,
   process.env.DB_PASSWORD,
   {
-    dialect: process.env.DB_DIALECT,
+    dialect: "mysql",
     host: process.env.DB_HOST,
   }
 );
